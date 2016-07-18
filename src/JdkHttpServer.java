a7de26281da4081e7538fdf10b7ee2d866c30713
Mirosław Filip <miroslaw.filip@schibsted.pl>
2016-07-18 15:22:29 +0200

    Rewrite context (url) handling in JdkHttpServer

--- src/JdkHttpServer.java
+++ src/JdkHttpServer.java
@@ -1,11 +1,17 @@
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer; // TODO: Is this good?
+import com.sun.net.httpserver.HttpServer;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 
 public final class JdkHttpServer implements Server {
@@ -17,13 +23,104 @@ public final class JdkHttpServer implements Server {
         this.application = application;
 
         server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/", new Handler())
-                .getFilters().add(new JdkHttpServerParamsFilter());
+
+        // TODO: Is there any sane way to serve static content with com.sun.net.httpserver?
+        server.createContext("/css/", new StaticContentHandler());
+
+        // TODO: With such approach, how to handle not existing paths?
+        JdkHttpServerParamsFilter paramExtractor = new JdkHttpServerParamsFilter();
+
+        server.createContext("/todos", new KnownContextHandler())
+                .getFilters().add(paramExtractor);
+
+        server.createContext("/toggleStatus", new KnownContextHandler())
+                .getFilters().add(paramExtractor);
+
+        server.createContext("/deleteTodo", new KnownContextHandler())
+                .getFilters().add(paramExtractor);
+
+        server.createContext("/clearTodo", new KnownContextHandler())
+                .getFilters().add(paramExtractor);
+
+        server.createContext("/", new DefaultHandler())
+                .getFilters().add(paramExtractor);
+
 
         server.setExecutor(null);
     }
 
-    private class Handler implements HttpHandler {
+    private class DefaultHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+            String url = httpExchange.getRequestURI().toString();
+
+            /**
+             * Redirect "/" to "/todos"
+             *
+             * For other resources, serve 404
+             */
+            if (url.equals("/")) {
+                // TODO: Change url if possible
+                new KnownContextHandler().handle(httpExchange);
+            } else {
+               return404(httpExchange);
+            }
+        }
+    }
+
+    private void return404(HttpExchange httpExchange) throws IOException {
+        String response = "Error 404 File not found.";
+
+        httpExchange.getResponseHeaders().set("Content-type", "text/plain");
+        httpExchange.sendResponseHeaders(404, response.length());
+
+        OutputStream output = httpExchange.getResponseBody();
+        output.write(response.getBytes());
+        output.flush();
+        output.close();
+    }
+
+    private class StaticContentHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+            try {
+                String method = httpExchange.getRequestMethod();
+                String url = httpExchange.getRequestURI().toString();
+
+                System.out.println(url + " requested");
+
+                if (method.equalsIgnoreCase("GET") && url.endsWith(".css")) {
+                    int lastDotIdx = url.lastIndexOf(".css");
+                    int lastSlash = url.lastIndexOf("/");
+                    String fileName = url.substring(lastSlash + 1, lastDotIdx + 4);
+
+                    String root = "src/";
+
+                    Path relativePath = Paths.get(root + fileName);
+                    if (Files.isReadable(relativePath)) {
+                        httpExchange.getResponseHeaders().set("Content-type", "text/css");
+                        httpExchange.sendResponseHeaders(200, 0);
+                        OutputStream output = httpExchange.getResponseBody();
+
+                        output.write(Files.readAllBytes(relativePath));
+                        output.flush();
+                        output.close();
+                    } else {
+                        return404(httpExchange);
+                    }
+                } else {
+                    return404(httpExchange);
+                }
+            } catch (NullPointerException e) {
+                httpExchange.getResponseHeaders().set("Content-type", "text/plain");
+                httpExchange.sendResponseHeaders(503, 0);
+            }
+        }
+    }
+
+    private class KnownContextHandler implements HttpHandler {
 
         @Override
         public void handle(HttpExchange httpExchange) throws IOException {
@@ -42,11 +139,35 @@ public final class JdkHttpServer implements Server {
                     httpExchange.getResponseHeaders().set("Location", "/");
                     httpExchange.sendResponseHeaders(303, -1);
                 } else {
-                    stringResponse = response.toString();
+                    HashMap<String, Object> scopes = new HashMap<String, Object>();
+
+                    Collection<Todo> allTodos = response.get("todos");
+                    Collection<Todo> completedTodos = response.get("completed");
+
+                    boolean areTodosEmpty = allTodos.isEmpty();
+                    int itemsLeftUndone = allTodos.size() - completedTodos.size();
+                    int itemsCompleted = completedTodos.size();
+                    boolean pluralTodos = itemsLeftUndone != 1;
+
+                    scopes.put("areTodosEmpty", areTodosEmpty);
+                    scopes.put("areAllCompleted", completedTodos.isEmpty());
+                    scopes.put("todosLeft", itemsLeftUndone);
+                    scopes.put("todosCompleted", itemsCompleted);
+                    scopes.put("pluralTodos", pluralTodos);
+                    scopes.put("allTodos", allTodos);
+                    scopes.put("todos", allTodos);
+                    scopes.put("completed", completedTodos);
+
+                    MustacheFactory mf = new DefaultMustacheFactory();
+
+                    Mustache mustache = mf.compile("template.mustache");
 
                     httpExchange.sendResponseHeaders(200, stringResponse.getBytes().length);
+
                     OutputStream os = httpExchange.getResponseBody();
-                    os.write(stringResponse.getBytes());
+                    Writer writer = new OutputStreamWriter(os);
+                    mustache.execute(writer, scopes).flush();
+
                     os.close();
                 }
             } catch (Exception e) {
